d93a8ea8d438f8c149269262c28196682150e248
kosiu1231 <aroczek1@wp.pl>
2024-03-02 11:37:13 +0100

    Add DataContext and ConnectionString

---
+++ Reforge/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿namespace Reforge.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Mod> Mods => Set<Mod>();
+        public DbSet<Comment> Comments => Set<Comment>();
+        public DbSet<Game> Games => Set<Game>();
+    }
+}
