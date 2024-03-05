e217c1ef0bc4659e8e58579ae51e77d3fda82a0d
kosiu1231 <aroczek1@wp.pl>
2024-03-05 16:30:51 +0100

    Add Comment dtos

---
+++ Reforge/Dtos/Comment/AddCommentDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Reforge.Dtos.Comment
+{
+    public class AddCommentDto
+    {
+        public string Text { get; set; } = string.Empty;
+        public int ModId { get; set; }
+    }
+}
---
+++ Reforge/Dtos/Comment/GetCommentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Reforge.Dtos.Comment
+{
+    public class GetCommentDto
+    {
+        public int Id { get; set; }
+        public string Text { get; set; } = string.Empty;
+        public int ModId { get; set; }
+    }
+}
